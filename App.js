4ddd190d515c71b78a171d3aad63f3974b67f649
pranaygoud22-hub <princepranay47@gmail.com>
2025-02-10 15:16:31 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,22 +1,25 @@
-import React from 'react';
-//import './App.css';
-/*import Welcome from './components/welcome';
-const App=()=>{
-  return(
-    <div style={{textAlign:'center',color:'green'}}>
-    <Welcome name='Rahul' greeting='Hello!!'/>
-    <Welcome name='sai' greeting='Hi!!'/>
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
-export default App;*/
-import Greeting from './components/Greeting';
-function App(){
-  const isLoggedIn=true;
-  return(
-    <div className='App'>
-      <Greetings isLoggedIn={isLoggedIn}/>
-    </div>
-  )
-}
+
 export default App;
